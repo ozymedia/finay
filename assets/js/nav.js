d88b4ba52d651972091df1f7748bf3eb9a7258de
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-12-24 22:36:50 +0100

    change

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,11 +1,10 @@
-console.log('bonjour');
-let x = "thomas";
-console.log(x);
 let button = document.getElementById("btn-nav");
 console.log(button);
 let sidenav = document.getElementById("side-menu");
+let nav = document.getElementById("nav");
 console.log(sidenav);
 button.addEventListener('click', (event) => {
   console.log(button);
   sidenav.classList.toggle("open");
 });
+
