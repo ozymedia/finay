dd64caf6d496ed492b19ec52b378c9fba813e2b1
thomas hunckler <hunckler.thomas@hotmail.fr>
2021-12-28 18:05:13 +0100

    change

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -16,3 +16,17 @@ window.addEventListener("scroll", (event) => {
   }
 });
 
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('form');
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => console.log('Form successfully submitted')).catch((error) =>
+    alert(error))
+}
+
