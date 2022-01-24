9c406c8763c4b5c9908a391a7e10c9b8570ecabf
thomas hunckler <hunckler.thomas@hotmail.fr>
2022-01-24 22:57:58 +0100

    change

--- assets/js/simulateur.js
+++ assets/js/simulateur.js
@@ -1,1 +1,15 @@
 console.log("simulateur");
+const bareme = {
+  1 :{
+    15262 : "bleu";
+    19565 : "jaune";
+    29148 : "violet";
+    29149 : "rose"
+  },
+  2 :{
+
+  },
+  3 :{},
+  4 :{},
+  5 :{}
+}
