85a3fdcb5f1185bfd9b532edf570309a20663eb3
thomas hunckler <hunckler.thomas@hotmail.fr>
2022-01-26 13:02:53 +0100

    change

--- assets/js/simulateur.js
+++ assets/js/simulateur.js
@@ -4,13 +4,13 @@ const bareme = {
     15262 : "bleu",
     19565 : "jaune",
     29148 : "violet",
-    29149 : "rose" // >29149
+    //29149 : "rose" // >29149
   },
   2 :{
     22320 : "bleu",
     28614 : "jaune",
     42848 : "violet",
-    42848 : "rose" // >42848
+    //42848 : "rose" // >42848
   },
   3 :{
     26844 : "bleu",
@@ -22,13 +22,13 @@ const bareme = {
     31359 : "bleu",
     40201 : "jaune",
     60336 : "violet",
-    60336 : "rose" // >60336
+    //60336 : "rose" // >60336
   },
   5 :{
     35894 : "bleu",
     46015 : "jaune",
     69081 : "violet",
-    69081 : "rose" // >69081
+    //69081 : "rose" // >69081
   }
 }
 const suppColors = {
@@ -46,7 +46,7 @@ document.getElementById("simulateur-prime-renov").addEventListener('submit', (ev
   if (people < 5) {
   bareme[people];
   let entries = Object.keys(bareme[people]);
-  let result = entries.filter(key => revenus < key)[0];
+  let result = entries.filter(key => revenus <= key)[0];
   let color = bareme[people][result] || "rose";
   console.log(color);
   document.getElementById("results").querySelector("#color").innerText = color;
