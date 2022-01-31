dbb3d28dd7a3aa0bce464efc3dfdabf8da00a808
thomas hunckler <hunckler.thomas@hotmail.fr>
2022-01-31 22:36:17 +0100

    change

--- assets/js/simulateur.js
+++ assets/js/simulateur.js
@@ -74,7 +74,7 @@ const isolationExt = {
   "jaune" : 60,
   "violet" : 40,
   "rose" : 15,
-  "plafon" : 150
+  "plafond" : 150
 }
 const paris = ["75","77","78","91","92","93","94","95"]
 
@@ -111,10 +111,10 @@ document.getElementById("simulateur-prime-renov").addEventListener('submit', (ev
     //let entries = Object.keys(bareme[people]);
     //let result = entries.filter(key => revenus <= key)[0];
     //let color = bareme[people][result] || "rose";
-    let aides = isolationExt[color] * surface;
+    let aides = isolationExt[color];
     console.log(localite);
     console.log(color);
-    if (travaux === 'no' || residence === 'no' || anciennete < 15 || occupation < 8) {
+    if (travaux === 'no' || residence === 'no') {
       document.getElementById("results").querySelector("#color").innerHTML = "";
       document.getElementById("results").querySelector("#aides").innerText = "";
       document.getElementById("results").querySelector("#aides").innerText = "Vous n'êtes pas éligible à MaPrimeRénov'";
@@ -124,7 +124,7 @@ document.getElementById("simulateur-prime-renov").addEventListener('submit', (ev
       pictoPrime.src = `${window.location.origin}/assets/images/icones/pictos-entreprise/prime-${color}.png`;
       color = color.replace(/^\w/, (c) => c.toUpperCase());
       document.getElementById("results").querySelector("#color").innerHTML = `Vous êtes éligible à <span class=${color}>MaPrimeRénov ${color}</span>`;
-      document.getElementById("results").querySelector("#aides").innerText = `Vous avez droit à ${aides} € d'aides avec MaPrimeRénov'`;
+      document.getElementById("results").querySelector("#aides").innerHTML = `Vous avez droit à ${aides}€/m<sup>2</sup> d'aides avec MaPrimeRénov'`;
     }
 
     document.querySelector('#scroll-to').scrollIntoView({
