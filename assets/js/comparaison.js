8788db45895a4874f14f03d115b02acba8d74219
thomas hunckler <hunckler.thomas@hotmail.fr>
2022-02-11 15:45:23 +0100

    change

--- assets/js/comparaison.js
+++ assets/js/comparaison.js
@@ -60,7 +60,7 @@ document.body.addEventListener('touchcancel',function(){
 
 function closebox(){
   let image = event.currentTarget.dataset.image;
-  document.querySelector(".lightbox").querySelector("img").src = `${window.location.origin}/assets/images/${image}`;
+  document.querySelector(".lightbox").querySelector("img").src = `${window.location.origin}/assets/images/realisations/${image}`;
   document.querySelector(".lightbox").classList.add("visible");
 }
 document.querySelector(".lightbox").addEventListener("click", (event) => {
