b07dff29abdb659ae7d7b0ef9830cd7713dac6cd
thomas hunckler <hunckler.thomas@hotmail.fr>
2023-04-09 11:42:12 +0200

    change json + text chauffage

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,18 +1,18 @@
-document.getElementById("popup").style.display = "none";
+//document.getElementById("popup").style.display = "none";
 
-function showStuff() {
-    document.getElementById("popup").style.display = "inline";
-}
+//function showStuff() {
+//    document.getElementById("popup").style.display = "inline";
+//}
 
-let linksNav = Array.from(document.querySelector("nav").querySelectorAll("a"));
-linksNav.forEach( element =>{
-    element.addEventListener('mouseover', (event) => {
-        showStuff();
-    });
-})
+//let linksNav = Array.from(document.querySelector("nav").querySelectorAll("a"));
+//linksNav.forEach( element =>{
+//    element.addEventListener('mouseover', (event) => {
+//        showStuff();
+//    });
+//})
 
-document.querySelector(".fa-circle-xmark").addEventListener("click", (event)=>{
-    document.getElementById("popup").remove();
-})
+//document.querySelector(".fa-circle-xmark").addEventListener("click", (event)=>{
+//    document.getElementById("popup").remove();
+//})
 
 //setTimeout(showStuff, 10000);
